8373262a7a0a3b50533a2d62b854d2f13d4a1f26
AnaCarolcs <anacarolinacsilva@outlook.com>
2019-10-24 09:03:58 -0300

    Aula 14

---
+++ Fabrilton/Aula14/pilha.c
@@ -0,0 +1,28 @@
+/*Considere o tipo pilha abaixo (que armazena idade de pessoas) e implemente as funções elementares para ela. Indique ainda qual a complexidade mínima (O()) de cada uma das funções.
+      struct TPilha{
+        int Topo;
+        intIdades[5];
+      };
+*/
+
+void Esvazia(TPilha *Pilha){  //Esvaziar a pilha;
+}
+
+bool Vazia(TPilha *Pilha){ //Verificar se a pilha está vazia ou não;
+}
+
+bool Empilha(TPilha*Pilha, TElemento *Elemento){ //Empilhar um elemento no topo da pilha;
+}
+
+Telemento *Desempilha(TPilha *Pilha){ //Desempilhar um elemento do topo da pilha –esta função retorna o elemento desempilhado;
+}
+
+Telemento  *Consulta(TPilha  *Pilha){ //Consulta:  retonar  o  elemento  do  topo  da  pilha  sem  removê-lo;
+}
+
+int main(){
+
+
+
+  return 0;
+}
---
+++ Fabrilton/Aula14/pilha2.c
@@ -0,0 +1,30 @@
+/*Considere o tipo pilha abaixo (que armazena altura de pessoas) e implemente as funções elementares para ela. Indique ainda qual a complexidade mínima (O()) de cada uma das funções.
+    struct TElemento{
+      float Altura;
+      TElemento *Prox;
+    };
+
+    TElemento *Pilha; //Apontador do topo da pilha
+*/
+
+void Esvazia(TPilha *Pilha){  //Esvaziar a pilha;
+}
+
+bool Vazia(TPilha *Pilha){ //Verificar se a pilha está vazia ou não;
+}
+
+bool Empilha(TPilha*Pilha, TElemento *Elemento){ //Empilhar um elemento no topo da pilha;
+}
+
+Telemento *Desempilha(TPilha *Pilha){ //Desempilhar um elemento do topo da pilha –esta função retorna o elemento desempilhado;
+}
+
+Telemento  *Consulta(TPilha  *Pilha){ //Consulta:  retonar  o  elemento  do  topo  da  pilha  sem  removê-lo;
+}
+
+int main(){
+
+
+
+  return 0;
+}
